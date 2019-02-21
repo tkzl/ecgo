90562f67efb21c2785879fb8422bb79457ebdf49
Tim <huangtiancai@bcb100.cn>
2019-02-21 11:14:55 +0800

    bugfix:buffer没起作用

--- util/log.go
+++ util/log.go
@@ -1,19 +1,21 @@
 //日志相关操作
 package util
 
+//TODO: access_log，buffer bugfix
+
 import (
 	"bufio"
 	"fmt"
 	"log"
 	"os"
-	"time"
 	"path/filepath"
+	"time"
 )
 
-var kinds  = []string {"trace","debug","warn","event","error"}
+var kinds = []string{"trace", "debug", "warn", "event", "error"}
 
 type LogMsg struct {
-	Time int64
+	Time    int64
 	Content string
 }
 type LogWriter interface {
@@ -21,8 +23,8 @@ type LogWriter interface {
 }
 
 type Logger struct {
-	writer map[string]LogWriter
-	buffc  map[string]chan string
+	writer   map[string]LogWriter
+	buffc    map[string]chan string
 	buffsize int
 }
 
@@ -33,7 +35,7 @@ func NewLogger(writer map[string]LogWriter, size ...int) *Logger {
 		writer = make(map[string]LogWriter)
 	}
 	for _, k := range kinds {
-		if _, ok := writer[k]; !ok {  
+		if _, ok := writer[k]; !ok {
 			writer[k] = defaultWriter
 		}
 	}
@@ -44,10 +46,12 @@ func NewLogger(writer map[string]LogWriter, size ...int) *Logger {
 	}
 	return &Logger{writer, buffc, buffsize}
 }
+
 //设置日志类型对应的writer
 func (this *Logger) SetWriter(kind string, w LogWriter) {
 	this.writer[kind] = w
 }
+
 //设置buffsize
 func (this *Logger) SetBufferSize(size int) {
 	this.buffsize = size
@@ -83,47 +87,52 @@ func (this *Logger) write(kind string, format string, vals ...interface{}) {
 	if this.writer[kind] == nil {
 		return
 	}
-	if _,ok := this.buffc[kind];!ok {
+	if _, ok := this.buffc[kind]; !ok {
 		this.buffc[kind] = make(chan string, this.buffsize)
-		go func (this *Logger, kind string) {
+		go func(this *Logger, kind string) {
 			msgs := []LogMsg{}
 			for {
 				select {
-					case msg := <- this.buffc[kind]:
-						msgs = append(msgs, LogMsg{Time:time.Now().Unix(), Content:msg})
-					case <- time.After(time.Millisecond * 500):
+				case msg := <-this.buffc[kind]:
+					msgs = append(msgs, LogMsg{Time: time.Now().Unix(), Content: msg})
+				case <-time.After(time.Second):
+					if len(msgs) > 0 { //指定时间没有新内容，发送已有内容
+						this.writer[kind].Write(kind, msgs)
+						msgs = msgs[0:0]
+					}
 				}
-				if len(msgs) > 0 {
+
+				if len(msgs) > this.buffsize {
 					this.writer[kind].Write(kind, msgs)
 					msgs = msgs[0:0]
 				}
 			}
-		} (this, kind)
+		}(this, kind)
 	}
-	this.buffc[kind] <- fmt.Sprintf(format + "\n", vals...)
+	this.buffc[kind] <- fmt.Sprintf(format+"\n", vals...)
 }
 
 // >######## 内置LogWriter
 
 //输出到console
-type ConsoleWriter struct{
+type ConsoleWriter struct {
 }
 
-func (*ConsoleWriter) Write (kind string, msg []LogMsg){
-	for _,v := range msg{
+func (*ConsoleWriter) Write(kind string, msg []LogMsg) {
+	for _, v := range msg {
 		fmt.Printf("%s[%5s]: %s", time.Unix(v.Time, 0).Format("2006/01/02 15:04:05 "), kind, v.Content)
 	}
 }
 
 //输出到文件
-type FileWriter struct{
+type FileWriter struct {
 	Path string
 	init bool
 }
 
-func (this *FileWriter) Write (kind string, msg []LogMsg){
+func (this *FileWriter) Write(kind string, msg []LogMsg) {
 	//初次调用，判断path
-	if !this.init { 
+	if !this.init {
 		path := filepath.Dir(this.Path)
 		if _, err := os.Stat(path); err != nil {
 			if os.IsNotExist(err) {
@@ -138,12 +147,12 @@ func (this *FileWriter) Write (kind string, msg []LogMsg){
 	dst := this.Path + "/" + kind
 	//判断是否需要rotate
 	stat, err := os.Stat(dst)
-	if err == nil { 
+	if err == nil {
 		//TODO: Size判断
 		modify := stat.ModTime().Format("20060102") //最后修改日期
 		today := time.Now().Format("20060102")
 		if today != modify {
-			os.Rename(dst, dst+"_"+ modify)
+			os.Rename(dst, dst+"_"+modify)
 		}
 	}
 	//写入
@@ -154,7 +163,7 @@ func (this *FileWriter) Write (kind string, msg []LogMsg){
 	}
 	defer fd.Close()
 	wFile := bufio.NewWriter(fd)
-	for _,v := range msg {
+	for _, v := range msg {
 		wFile.WriteString(fmt.Sprintf("%s %s", time.Unix(v.Time, 0).Format("2006/01/02 15:04:05 "), v.Content))
 	}
 	wFile.Flush()
